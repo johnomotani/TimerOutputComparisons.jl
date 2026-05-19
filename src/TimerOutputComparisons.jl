903f04a701bc722be4b9a997d85ed595d78e0464
John Omotani <john.omotani@ukaea.uk>
2026-05-19 14:32:33 +0100

    Initial implementation

--- src/TimerOutputComparisons.jl
+++ src/TimerOutputComparisons.jl
@@ -1,5 +1,281 @@
+"""
+TimerOutputComparisons
+======================
+
+Provides some helper functions to save/load TimerOutput objects, and plot comparisons of
+them. This may be useful to compare performance with different settings, or between
+different versions of some code.
+"""
 module TimerOutputComparisons
 
-# Write your package code here.
+export save_timer, load_timer, compare_timers
+# Workaround for failures when JLD is not loaded in Main, see
+# https://github.com/JuliaIO/JLD.jl/issues/252
+export JLD
+
+using GLMakie
+using JLD
+using TimerOutputs
+
+"""
+    save_timer(filename::AbstractString, to::TimerOutput,
+               timer_name::AbstractString="to")
+
+Save `to` to a JLD file called `filename`, in a variable called `timer_name`. `filename`
+should end with ".jld".
+"""
+function save_timer(filename::AbstractString, to::TimerOutput,
+                    timer_name::AbstractString="to")
+    if splitext(filename)[2] != ".jld"
+        error("`filename` should end in \".jld\" so that JLD format is used. Otherwise "
+              * "a TimerOutput might not be writable and re-loadable.")
+    end
+    JLD.save(filename, timer_name, to)
+end
+
+"""
+    load_timer(filename::AbstractString,
+               timer_name::AbstractString="to")::TimerOutput
+
+Load a TimerOutput called `timer_name` from a JLD file called `filename`.
+"""
+function load_timer(filename::AbstractString,
+                    timer_name::AbstractString="to")::TimerOutput
+    if splitext(filename)[2] != ".jld"
+        error("Expected `filename` to end in \".jld\" so that JLD format is used.")
+    end
+    to = JLD.load(filename, timer_name)
+    return to
+end
+
+const possible_use_data = (:ncalls, :time, :allocs)
+
+"""
+    compare_timers(timers::Union{AbstractString,Tuple{<:AbstractString,<:AbstractString},Tuple{TimerOutput,<:AbstractString}}...;
+                   flatten::Bool=false, averages::Bool=false,
+                   save_as::Union{AbstractString,Nothing}=nothing,
+                   use_data=possible_use_data, legend::Bool=true, root=nothing)
+
+Make a plot comparing `timers`. For `t` in `timers`:
+* if `t` is an AbstractString, load a TimerOutput from the file named `t` using
+  `load_timer()`, and label it `t`.
+* if `t` is a `Tuple{<:AbstractString,<:AbstractString}`, load a TimerOutput called `t[2]`
+  from the file named `t[1]` using `load_timer()`, and label it `t[1] * ":" * t[2]`.
+* if `t` is a `Tuple{TimerOutput,<:AbstractString}`, use the TimerOutput `t[1]` and label
+  it `t[2]`.
+
+We assume that the TimerOutput objects in `timers` contain (mostly) the same timers,
+otherwise this comparison will not make much sense.
+
+If `flatten=true`, the TimerOutput objects are flattened with `TimerOutputs.flatten()`.
+
+If `averages=true`, then the time per call and allocs per call will be plotted instead of
+the total time and total allocs for each timer.
+
+If a file-name is passed to `save_as` the plots are saved instead of being displayed
+interactively. For example, `save_as="foo.png"` would result in the plots being saved as
+"foo_ncalls.png", "foo_time.png", and "foo_allocs.png".
+
+To plot only one or two quantities, pass one or more of `:ncalls`, `:time` and `:allocs`
+to `use_data`, for example `use_data=:time`.
+
+To remove the legend, pass `legend=false`.
+
+If you only want to use_data some (possibly nested) sub-timer, pass its name as a String,
+Tuple or Vector to `root`. For example if the top-level TimerOutput is `to`, passing
+`root=("foo", "bar")` will use_data only the timers in `to["foo"]["bar"]`.
+"""
+compare_timers
+
+function compare_timers(timers::Union{AbstractString,<:Tuple{AbstractString,AbstractString},<:Tuple{TimerOutput,AbstractString}}...; kwargs...)
+    function get_timer(t)::Tuple{TimerOutput,String}
+        if t isa Tuple{TimerOutput,<:AbstractString}
+            return (t[1], String(t[2]))
+        elseif t isa AbstractString
+            return (load_timer(t), splitext(String(t))[1])
+        elseif t isa Tuple{<:AbstractString,<:AbstractString}
+            return (load_timer(t...), splitext(String(t[1]))[1] * ":" * String(t[2]))
+        else
+            error("Unsupported type $(typeof(t)) for t=$t.")
+        end
+    end
+    return compare_timers(Tuple(get_timer(t) for t ∈ timers)...; kwargs...)
+end
+function compare_timers(timers::Tuple{TimerOutput,String}...;
+                        flatten::Bool=false, averages::Bool=false,
+                        save_as::Union{AbstractString,Nothing}=nothing,
+                        use_data=possible_use_data, legend::Bool=true, root=nothing)
+
+    if isa(use_data, Symbol)
+        use_data = (use_data,)
+    end
+    for i ∈ use_data
+        if i ∉ possible_use_data
+            error("'$i' is not a valid entry in use_data. Possible values are "
+                  * "$possible_use_data.")
+        end
+    end
+
+    to_list = [t[1] for t ∈ timers]
+    x_values = [t[2] for t ∈ timers]
+
+    if flatten
+        to_list = [TimerOutputs.flatten(t) for t ∈ to_list]
+    end
+    if root !== nothing
+        if isa(root, AbstractString)
+            root = [root]
+        end
+        for r ∈ root
+            to_list = [t[r] for t ∈ to_list]
+        end
+    end
+
+    # Get names of all timers to plot.
+    timer_names = Vector{String}[]
+    function extract_names!(t, name)
+        if !isempty(name) && name ∉ timer_names
+            push!(timer_names, name)
+        end
+        inner_timers = t.inner_timers
+        if !isempty(inner_timers)
+            for k ∈ keys(inner_timers)
+                new_name = copy(name)
+                push!(new_name, k)
+                extract_names!(t[k], new_name)
+            end
+        end
+        return nothing
+    end
+    for t ∈ to_list
+        extract_names!(t, String[])
+    end
+
+    xticks = (1:length(x_values), x_values)
+    if :ncalls ∈ use_data
+        fig_ncalls = Figure()
+        ax_ncalls = Axis(fig_ncalls[1,1]; xticks=xticks, ylabel="ncalls")
+    else
+        fig_ncalls = nothing
+        ax_ncalls = nothing
+    end
+    if :time ∈ use_data
+        fig_time = Figure()
+        ax_time = Axis(fig_time[1,1]; xticks=xticks, ylabel="time (ms)")
+    else
+        fig_time = nothing
+        ax_time = nothing
+    end
+    if :allocs ∈ use_data
+        fig_allocs = Figure()
+        ax_allocs = Axis(fig_allocs[1,1]; xticks=xticks, ylabel="allocated (kB)")
+    else
+        fig_allocs = nothing
+        ax_allocs = nothing
+    end
+
+    if root !== nothing
+        plot_single_timer!(ax_ncalls, ax_time, ax_allocs, to_list, root[end:end], xticks,
+                           averages, true)
+    end
+    for name ∈ timer_names
+        plot_single_timer!(ax_ncalls, ax_time, ax_allocs, to_list, name, xticks, averages)
+    end
+
+    if legend
+        for (fig, ax) in zip((fig_ncalls, fig_time, fig_allocs), (ax_ncalls, ax_time, ax_allocs))
+            if fig !== nothing
+                # Ensure the first row width is 3/4 of the column width so that the plot does not get
+                # squashed by the legend
+                rowsize!(fig.layout, 1, Aspect(1, 3/4))
+
+                Legend(fig[2,1], ax; tellwidth=false, tellheight=true)
+
+                resize_to_layout!(fig)
+            end
+        end
+    end
+
+    if save_as === nothing
+        backend = Makie.current_backend()
+        for fig in (fig_ncalls, fig_time, fig_allocs)
+            if fig !== nothing
+                DataInspector(fig)
+                display(backend.Screen(), fig)
+            end
+        end
+    else
+        prefix, suffix = splitext(save_as)
+        if fig_ncalls !== nothing
+            save(prefix * "_ncalls" * suffix, fig_ncalls)
+        end
+        if fig_time !== nothing
+            save(prefix * "_time" * suffix, fig_time)
+        end
+        if fig_allocs !== nothing
+            save(prefix * "_allocs" * suffix, fig_allocs)
+        end
+    end
+
+    return fig_ncalls, fig_time, fig_allocs
+end
+
+function get_single_timer(to::TimerOutput, name::Vector{String})
+    for n ∈ name
+        if n ∈ keys(to.inner_timers)
+            to = to[n]
+        else
+            return nothing
+        end
+    end
+    return to
+end
+
+function plot_single_timer!(ax_ncalls, ax_time, ax_allocs, to_list, name::Vector{String},
+                            xticks, averages, is_root::Bool=false)
+    if is_root
+        this_timer_list = to_list
+    else
+        this_timer_list = [get_single_timer(to, name) for to ∈ to_list]
+    end
+    label = join(name, ":")
+
+    if averages
+        ncalls_values = [t === nothing ? 0 : TimerOutputs.ncalls(t) for t ∈ this_timer_list]
+    end
+
+    xtick_values = xticks[2]
+    if ax_ncalls !== nothing
+        if !averages
+            ncalls_values = [t === nothing ? 0 : TimerOutputs.ncalls(t) for t ∈ this_timer_list]
+        end
+        lines!(ax_ncalls, ncalls_values;
+               label,
+               inspector_label=(self,i,p) -> "$(self.label[])\n$(xtick_values[round(Int64, p[1])]): ncalls=$(ncalls_values[round(Int64, p[1])])")
+    end
+
+    # Convert times from ns to ms.
+    if ax_time !== nothing
+        time_values = [t === nothing ? NaN : TimerOutputs.time(t) * 1.0e-6 for t ∈ this_timer_list]
+        if averages
+            time_values ./= ncalls_values
+        end
+        lines!(ax_time, time_values;
+               label,
+               inspector_label=(self,i,p) -> "$(self.label[])\n$(xtick_values[round(Int64, p[1])]): time=$(time_values[round(Int64, p[1])]) ms")
+    end
+
+    if ax_allocs !== nothing
+        allocs_values = [t === nothing ? NaN : TimerOutputs.allocated(t) / 1024 for t ∈ this_timer_list]
+        if averages
+            allocs_values ./= ncalls_values
+        end
+        lines!(ax_allocs, allocs_values;
+               label,
+               inspector_label=(self,i,p) -> "$(self.label[])\n$(xtick_values[round(Int64, p[1])]): allocs=$(allocs_values[round(Int64, p[1])]) kB")
+    end
+
+    return nothing
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,61 @@
 using TimerOutputComparisons
+using TimerOutputs
 using Test
 
-@testset "TimerOutputComparisons.jl" begin
-    # Write your tests here.
+function dump_timer(sleeptime, outputdir)
+    to = TimerOutput()
+    @timeit to "sleep" sleep(sleeptime)
+    filename = joinpath(outputdir, "foo$sleeptime.jld")
+    save_timer(filename, to)
 end
+function dump_timer(sleeptime, outputdir, timer_name)
+    to = TimerOutput()
+    @timeit to "sleep" sleep(sleeptime)
+    filename = joinpath(outputdir, "foo$sleeptime.jld")
+    save_timer(filename, to, timer_name)
+end
+
+function runtests()
+    @testset "TimerOutputComparisons.jl" begin
+        @testset "flatten=$flatten, averages=$averages, legend=$legend root=$root, use_data=$use_data" for
+                flatten ∈ (false, true), averages ∈ (false, true), legend ∈ (true, false),
+                root ∈ (nothing, ["first_level", "second_level"]),
+                use_data ∈ (nothing, :ncalls, :time, :allocs, (:ncalls, :time),
+                            (:ncalls, :allocs), (:time, :allocs),
+                            (:allocs, :time, :ncalls))
+            # This package mostly makes interactive plots, so this is primarily a smoke-test.
+            outputdir = tempname()
+            mkpath(outputdir)
+
+            dump_timer(0.1, outputdir)
+            dump_timer(0.5, outputdir, "bar")
+
+            to = TimerOutput()
+            @timeit to "sleep" sleep(1)
+            @timeit to "first level" begin
+                @timeit to "second level" begin
+                    @timeit to "sleep" sleep(0.2)
+                end
+            end
+
+            @test isa(load_timer(joinpath(outputdir, "foo0.1.jld")), TimerOutput)
+            @test isa(load_timer(joinpath(outputdir, "foo0.5.jld"), "bar"), TimerOutput)
+
+            if use_data === nothing
+                compare_timers(joinpath(outputdir, "foo0.1.jld"),
+                               (joinpath(outputdir, "foo0.5.jld"), "bar"),
+                               (to, "foo1");
+                               flatten, averages, legend,
+                               save_as=joinpath(outputdir, "foo.png"))
+            else
+                compare_timers(joinpath(outputdir, "foo0.1.jld"),
+                               (joinpath(outputdir, "foo0.5.jld"), "bar"),
+                               (to, "foo1");
+                               flatten, averages, legend, use_data,
+                               save_as=joinpath(outputdir, "foo.png"))
+            end
+        end
+    end
+end
+
+runtests()
